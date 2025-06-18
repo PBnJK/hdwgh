0a6c43afb299202b75d28e14d4f7dde9137adbdf
PBnJK <pedrobuitrago2007yahoo@gmail.com>
2025-06-18 11:17:12 -0300

    citation finding pt. 1

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/pbnjk/hdwgh
 
-go 1.23.1
+go 1.24.1
+
+toolchain go1.24.4
+
+require github.com/ledongthuc/pdf v0.0.0-20250511090121-5959a4027728 // indirect
--- internal/article/article.go
+++ internal/article/article.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"regexp"
+
+	"github.com/ledongthuc/pdf"
 )
 
 type Article struct {
@@ -26,8 +29,7 @@ func New(input string) (Article, error) {
 	defer file.Close()
 
 	fetchPDF(file, input)
-
-	return Article{}, nil
+	return createArticle(file)
 }
 
 func createTemporaryFile() (*os.File, error) {
@@ -63,12 +65,115 @@ func downloadFromURL(file *os.File, url string) error {
 
 	defer response.Body.Close()
 
-	fmt.Printf("%s\n", response.Status)
-
 	_, err = io.Copy(file, response.Body)
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func createArticle(file *os.File) (Article, error) {
+	fi, err := file.Stat()
+	if err != nil {
+		return Article{}, err
+	}
+
+	fmt.Printf("Creating PDF reader... ")
+	r, err := pdf.NewReader(file, fi.Size())
+	if err != nil {
+		fmt.Println("ERROR")
+		return Article{}, err
+	}
+	fmt.Println("OK!")
+
+	article := Article{
+		sources: make([]string, 0),
+	}
+
+	pageNumber := r.NumPage()
+
+	// Loop back through pages, since references/bibliographies are generally at
+	// the end of an article
+	for pageNumber > 0 {
+		fmt.Printf("Reading page %d... ", pageNumber)
+		page := r.Page(pageNumber)
+		if page.V.IsNull() || page.V.Key("Contents").Kind() == pdf.Null {
+			fmt.Println("no content! Skipping...")
+
+			pageNumber--
+			continue
+		}
+		fmt.Println("OK!")
+
+		fmt.Printf("Finding sources... ")
+		sources, err := getPageSources(page)
+		if err != nil {
+			fmt.Println("ERROR")
+			return Article{}, nil
+		}
+		fmt.Println("OK!")
+
+		// No sources found, assume we're done
+		if len(sources) == 0 {
+			break
+		}
+
+		article.sources = append(article.sources, sources...)
+
+		pageNumber--
+		break
+	}
+
+	return article, nil
+}
+
+func getPageSources(page pdf.Page) ([]string, error) {
+	t, err := page.GetPlainText(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	sources := make([]string, 0)
+
+	sources = append(sources, findDOI(t)...)
+	sources = append(sources, findArxiv(t)...)
+
+	return sources, nil
+}
+
+func findDOI(text string) []string {
+	sources := make([]string, 0)
+
+	// TODO:
+	// Find identifiers in the form: doi:10.nnnn/nnnn...
+
+	// doiRegex := regexp.MustCompile(`10\..+?/.+`)
+
+	return sources
+}
+
+func findArxiv(text string) []string {
+	sources := make([]string, 0)
+
+	// TODO:
+	// Find identifiers in the form: arXiv:nnnn.nnnnn
+
+	arxivRegex := regexp.MustCompile(`arXiv:(\d{4}\.\d+)`)
+	for _, match := range arxivRegex.FindAllString(text, -1) {
+		fmt.Println(match)
+	}
+
+	return sources
+}
+
+func findURLs(text string) []string {
+	sources := make([]string, 0)
+
+	// TODO:
+	// Find and disambiguate URLs in the forms:
+	// - arxiv.org/abs/nnnn.nnnnn, arxiv.org/pdf/nnnn.nnnnn as ArXiv articles
+	// - doi.org/10.nnnn/nnnn... as DOI articles
+
+	return sources
 }
